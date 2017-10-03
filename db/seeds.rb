46b64561f2a0a7341fbf5dc7174c55719a6f06ad
Christian Dacres <christian_dacres@hotmail.co.uk>
2017-10-03 20:21:57 +0100

    seeded, added script to models and tested

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+c1 = Cat.create!(name: "Nox", breed: "Manx", age: 6)
+c2 = Cat.create!(name: "Fuzzles", breed: "Turkish Van", age: 2)
+c3 = Cat.create!(name: "Mr. Fopadopalis", breed: "Bengal", age: 10)
+c4 = Cat.create!(name: "Crookshanks", breed: "Half-Kneazle", age: 5)
+
+h1 = Home.create!(address: "4 Privet Drive", house_type: "Semi-Detached", occupants: 3)
+h2 = Home.create!(address: "221B Baker Street", house_type: "Terrace", occupants: 2)
+h3 = Home.create!(address: "32 Windsor Gardens", house_type: "Terrace", occupants: 5)
+h4 = Home.create!(address: "Longbourn House", house_type: "Detached", occupants: 7)
+
+CatHome.create!(cat_id: c1.id, home_id: h2.id)
+CatHome.create!(cat_id: c1.id, home_id: h3.id)
+CatHome.create!(cat_id: c2.id, home_id: h4.id)
+CatHome.create!(cat_id: c3.id, home_id: h1.id)
+CatHome.create!(cat_id: c3.id, home_id: h2.id)
+CatHome.create!(cat_id: c4.id, home_id: h1.id)
+CatHome.create!(cat_id: c4.id, home_id: h3.id)
